25674975aca16ef205c640397c0efffff9cc7f00
Amir <amir-rt@live.com>
2021-09-13 21:29:04 +0200

    feat: prompter

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log(`Hello world!`);
+import inquirer from "inquirer";
+
+const prompt = inquirer.createPromptModule();
+
+prompt([
+  { type: "input", message: "number of test cases?", name: "entry0" },
+  {
+    type: "input",
+    message: "how many rows and columns (space sperated)",
+    name: "entry1",
+  },
+]).then((answers) => {
+  const listOfInput = [];
+  for (let i = 0; i < answers.entry0; i++) {
+    listOfInput.push({
+      type: "input",
+      message: "insert numbers indicating black and white",
+      name: "row" + i,
+    });
+  }
+  prompt(listOfInput).then((answers) => {
+    console.log(answers);
+  });
+});
